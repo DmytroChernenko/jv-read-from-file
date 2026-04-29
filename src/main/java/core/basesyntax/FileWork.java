6d2fb69ae691f53ed2f01478cb289a82d6dea0df
mitresko <mitresko12@gmail.com>
2026-04-29 17:18:52 +0300

    fixed checkstyle and optimize imports

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,24 +1,26 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class FileWork {
     public String[] readFromFile(String fileName) {
         //write your code here
         String[] result = new String[0];
+
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
-            String[] words = bufferedReader.lines().collect(Collectors.joining("\n")).split("\\W+");
-            List<String> wordsList = Arrays.stream(words)
+            String[] words = bufferedReader.lines()
+                    .collect(Collectors.joining("\n"))
+                    .split("\\W+");
+            result = Arrays.stream(words)
                     .map(String::toLowerCase)
                     .filter(word -> word.startsWith("w"))
                     .sorted()
-                    .toList();
-            result = wordsList.toArray(new String[0]);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+                    .toList()
+                    .toArray(new String[0]);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
